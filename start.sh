1d7e9e7d91ee3312793f74fa1deede86c421a169
Aman jii <151424799+Botsthe@users.noreply.github.com>
2024-01-13 17:56:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Botsthe/Aa.git /Aa
+  git clone https://github.com/Botsthe/Di.git /Di
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Aa
+  git clone $UPSTREAM_REPO /Di
 fi
-cd /Aa
+cd /Di
 pip3 install -U -r requirements.txt
 echo "Starting Aa 😎...."
 python3 bot.py    
